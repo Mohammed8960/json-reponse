144d8fcba3817cc7f27649569f67b7f49fa9450f
Mohammed Abdelrasoul Ismail <mrasoul@modern-electronics.com>
2020-05-22 06:50:34 +0300

    initial setup

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+require __DIR__."/vendor/autoload.php";
+
+use Devscreencast\ResponseClass\JsonResponse;
+
+$student = array('name'=>'Moha','course'=>'PHP','level'=>'basic');
+
+new JsonResponse('ok','',$student);
