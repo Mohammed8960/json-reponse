88748b21642081b82271973ca908a36cbce09643
Mohammed Abdelrasoul Ismail <mrasoul@modern-electronics.com>
2020-05-22 07:00:17 +0300

    updating composer

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ require __DIR__."/vendor/autoload.php";
 
 use Devscreencast\ResponseClass\JsonResponse;
 
-
 $student = array('name'=>'Moha','course'=>'PHP','level'=>'basic');
 
 new JsonResponse('ok','',$student);
